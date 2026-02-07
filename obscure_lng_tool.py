0d1a21f2ea68683938e70dd5de8f580849ae3db8
Kapie77 <usergames02@proton.me>
2026-02-07 03:32:25 -0300

    Update obscure_lng_tool.py

--- obscure_lng_tool.py
+++ obscure_lng_tool.py
@@ -48,9 +48,6 @@ MAP_OB1 = {
 # OBSCURE 2
 # Tabela de glifos da fonte do Obscure 2
 """
-Olhei as texturas do Obscure 2 e eis as fontes:
-(manda o map consertado)
-
 font_smallfonts7.tga:
 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 a b c d e f g h i j k l m n o p q r s t u v w x y z
@@ -577,4 +574,5 @@ if __name__ == '__main__':
         # Passou argumentos → CLI normal
 
         main()
+
 
